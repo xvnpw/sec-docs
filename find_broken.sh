2af6836d2d377a29a0fa10f65d2337111d740c45
xvnpw <17719543+xvnpw@users.noreply.github.com>
2024-12-28 15:36:00 +0100

    script

--- find_broken.sh
+++ find_broken.sh
@@ -2,6 +2,14 @@ TMP_FILE=$(mktemp)
 
 find "." -type f -name "*.md" -size -4 -print > $TMP_FILE
 
+find . -type f -iname "*.md" | while read -r file; do
+    whitespaces=$(grep -o '[[:space:]]' "$file" | wc -l)
+    non_whitespaces=$(grep -o '[^[:space:]]' "$file" | wc -l)
+    if [ "$whitespaces" -gt "$non_whitespaces" ]; then
+        echo $file >> $TMP_FILE
+    fi
+done
+
 cat $TMP_FILE | while read line; do
     REPO=$(echo $line | cut -d'/' -f3)
     grep $REPO origin_repos.txt >> redo_repos.txt
