dd5b22d8b767b55cc11afdc9f04ffee5d06b7bf8
xvnpw <17719543+xvnpw@users.noreply.github.com>
2024-12-28 15:03:39 +0100

    repos

--- find_broken.sh
+++ find_broken.sh
@@ -1,1 +1,14 @@
-find "." -type f -name "*.md" -size -3 -print
+TMP_FILE=$(mktemp)
+
+find "." -type f -name "*.md" -size -4 -print > $TMP_FILE
+
+cat $TMP_FILE | while read line; do
+    REPO=$(echo $line | cut -d'/' -f3)
+    grep $REPO origin_repos.txt >> redo_repos.txt
+done
+
+# cat $TMP_FILE | while read line; do
+#     echo $line | xargs -I {} rm -r {}
+# done
+
+rm $TMP_FILE
