52fa30b866bcaf13cea31d26d8c49dbe183ff730
xvnpw <17719543+xvnpw@users.noreply.github.com>
2024-12-28 15:59:19 +0100

    update

--- find_broken.sh
+++ find_broken.sh
@@ -1,6 +1,6 @@
 TMP_FILE=$(mktemp)
 
-find "." -type f -name "*.md" -size -4 -print > $TMP_FILE
+# find "." -type f -name "*.md" -size -4 -print > $TMP_FILE
 
 find . -type f -iname "*.md" | while read -r file; do
     whitespaces=$(grep -o '[[:space:]]' "$file" | wc -l)
@@ -12,7 +12,8 @@ done
 
 cat $TMP_FILE | while read line; do
     REPO=$(echo $line | cut -d'/' -f3)
-    grep $REPO origin_repos.txt >> redo_repos.txt
+    TYPE=$(echo $line | cut -d'/' -f2)
+    grep -E "$TYPE.*/$REPO" origin_repos.txt >> redo_repos.txt
 done
 
 cat redo_repos.txt | sort -u -o redo_repos.txt
